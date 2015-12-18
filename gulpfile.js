c1fbf99f1fd8f53509fb20ea627e03d6fbbd7d70
Facundo Victor <facundovt@gmail.com>
2015-12-18 13:52:33 -0300

    Added gulp tasks for stylus and jade
    New tasks:
            - watch: Wathes the source and rebuilds the project.
            - connect: Serves the page on localhost:8080 and launches a
                       a chrome instance for debugging purposes.
            - coffe_reload: Compiles CoffeScript and reloads the connec
                            -tion.
            - stylus: Compiles Stylus.
            - stylus_reload: Compiles Stylus and reloads the server.
            - jade: Compiles Jade.
            - jade_reload: Compiles Jade and reloads the connection.
            - serve: Builds the page, serves the page on localhost:8080
                     and launches a chrome instance for debugging.
            - reloadable_build: Builds the project and reloads the
                                server connection.

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,44 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    stylus = require('gulp-stylus'),
+    poststylus = require('poststylus'),
+    cssnano = require('gulp-cssnano'),
+    jade = require('gulp-jade'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    lost = require('lost');
 
 // Specific Tasks -------------------------------------------------------------
 
-// CoffeeScript compilation task
+// Server connection task
+var connect = require('gulp-connect-multi')();
+gulp.task('connect', connect.server({
+    root: ['public'],
+    port: 8080,
+    livereload: true,
+    open: {
+        browser: 'google-chrome'
+    }
+}));
+
+// COFESCRIPT -----------------------------------------------------------------
+
+// CoffeeScript compilation task with connection reload
 var coffee_src = './src/coffee/**/*.coffee';
+gulp.task('coffee_reload', function() {
+    gulp.src(coffee_src)
+        .pipe(sourcemaps.init())
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./public/js/'))
+        .pipe(connect.reload());
+});
+
+// CoffeeScript compilation task
 gulp.task('coffee', function() {
     gulp.src(coffee_src)
         .pipe(coffee({bare: true}).on('error', gutil.log))
@@ -22,12 +54,91 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('./public/js/'));
 });
 
-// ----------------------------------------------------------------------------                                                                                                             
-// GLOBAL TASKS 
+// ----------------------------------------------------------------------------
+// JADE
+
+// Jade compilation task with connection reload
+var jade_src = './src/jade/**/*.jade';
+gulp.task('jade_reload', function(){
+    gulp.src(jade_src)
+        .pipe(jade({
+            locals: {},
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'))
+        .pipe(connect.reload());
+});
+
+// Jade compilation task
+gulp.task('jade', function(){
+    gulp.src(jade_src)
+        .pipe(jade({
+            locals: {},
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'));
+});
+// ----------------------------------------------------------------------------
+// STYLUS
+
+// Stylus compilation task with connection reload
+var stylus_src = './src/stylus/**/*.styl';
+gulp.task('stylus_reload', function () {
+    gulp.src(stylus_src)
+        .pipe(sourcemaps.init())
+        .pipe(stylus({
+            use: [
+                poststylus(['autoprefixer', 'lost'])
+            ]
+        }))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./map'))
+        .pipe(gulp.dest('./public/css/'))
+        .pipe(connect.reload());
+});
+
+// Stylus compilation task
+var stylus_src = './src/stylus/**/*.styl';
+gulp.task('stylus', function () {
+    gulp.src(stylus_src)
+        .pipe(stylus({
+            use: [
+                poststylus(['autoprefixer', 'lost'])
+            ]
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./public/css/'));
+});
+
+// ----------------------------------------------------------------------------
+// GLOBAL TASKS
+
+// Watch task
+gulp.task('watch', function () {
+    gulp.watch([coffee_src], ['coffee']);
+    gulp.watch([jade_src], ['jade']);
+    gulp.watch([stylus_src], ['stylus']);
+});
 
 // Simple build
 gulp.task('build', [
-    'coffee'
+    'coffee',
+    'jade',
+    'stylus'
+]);
+
+// Reloadable build, requires a previous connection
+gulp.task('reloadable_build', [
+    'coffee_reload',
+    'jade_reload',
+    'stylus_reload'
+]);
+
+// Build, Launch a server and watch for fast reload
+gulp.task('serve', [
+    'connect',
+    'reloadable_build',
+    'watch'
 ]);
 
 gulp.task('default', ['build']);
